93e36a02a854914599835c205286658d911a38f5
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2025-07-10 21:33:10 +0200

    feat: make command dialog responsive

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,9 +5,6 @@ import {TRPCError} from "@trpc/server";
 
 export const agentRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
-
-    throw new TRPCError({message: "UPS", code: "BAD_REQUEST"});
-
     return db.select().from(agent);
   })
 })
